f2db9e7177f0e8ff78637a004c43c6cd8cb62127
minjae <wjsalswp303@gmail.com>
2025-02-04 12:29:05 +0900

    [INIT]: 프로젝트 초기 세팅

---
+++ src/main/java/hongmumuk/hongmumuk/HongmumukApplication.java
@@ -0,0 +1,13 @@
+package hongmumuk.hongmumuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongmumukApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HongmumukApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hongmumuk/hongmumuk/HongmumukApplicationTests.java
@@ -0,0 +1,13 @@
+package hongmumuk.hongmumuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongmumukApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
