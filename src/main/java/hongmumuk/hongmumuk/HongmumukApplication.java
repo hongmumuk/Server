8d51449ce95a817345a302b43e7b39d8d30e43fd
jhchoi316 <chlwogns316@gmail.com>
2025-02-05 15:19:28 +0900

    [REFACTOR]: Application

--- src/main/java/hongmumuk/hongmumuk/HongmumukApplication.java
+++ src/main/java/hongmumuk/hongmumuk/HongmumukApplication.java
@@ -8,7 +8,6 @@ public class HongmumukApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HongmumukApplication.class, args);
-        System.out.println("Hongmumuk Application Started");
     }
 
 }
