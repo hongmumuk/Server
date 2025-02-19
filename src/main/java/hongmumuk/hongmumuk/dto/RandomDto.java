f90ef6817bbe6c887194a9cdf51746b2edd15f42
jhchoi316 <chlwogns316@gmail.com>
2025-02-19 17:46:13 +0900

    [FEATURE]: 랜덤 기능 구현

---
+++ src/main/java/hongmumuk/hongmumuk/dto/RandomDto.java
@@ -0,0 +1,19 @@
+package hongmumuk.hongmumuk.dto;
+
+import hongmumuk.hongmumuk.entity.Restaurant;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RandomDto {
+    private Long id;
+    private String name;
+
+    public static RandomDto from(Restaurant restaurant) {
+        return new RandomDto(
+                restaurant.getId(),
+                restaurant.getName()
+        );
+    }
+}
--- src/main/java/hongmumuk/hongmumuk/repository/RestaurantRepository.java
+++ src/main/java/hongmumuk/hongmumuk/repository/RestaurantRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     Page<Restaurant> findAllByCategory(Category category, Pageable pageable);
+
 }
