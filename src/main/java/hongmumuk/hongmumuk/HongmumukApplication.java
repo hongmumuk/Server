f7e7e3dadd44dca59ca8e0d37f9b1ade17afe841
jhchoi316 <chlwogns316@gmail.com>
2025-02-25 15:55:50 +0900

    [FEATURE]: 기능 구현

--- src/main/java/hongmumuk/hongmumuk/HongmumukApplication.java
+++ src/main/java/hongmumuk/hongmumuk/HongmumukApplication.java
@@ -1,9 +1,12 @@
 package hongmumuk.hongmumuk;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://hongmumuk.shop", description = "Default Server url")})
 @EnableScheduling
 @SpringBootApplication
 public class HongmumukApplication {
